22a738b3d21beb4c9bff9dad88d029e9495aefd2
novtech06 <mn5475081@gmail.com>
2023-02-15 13:52:27 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import '@/styles/globals.css'
 import i18n from '@/i18n'
 import { useRouter } from 'next/router'
-import { appWithTranslation } from 'next-i18next'
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const router = useRouter()
   if (router.locale) {
     i18n.changeLanguage(router.locale)
@@ -11,5 +10,3 @@ function App({ Component, pageProps }) {
   
   return <Component {...pageProps} />
 }
-
-export default appWithTranslation(App)
